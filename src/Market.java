47bb9d21d5be85882cba8d3d13dd29a1fdaed08c
Abhinav Srivastava <atg271@gmail.com>
2022-11-09 17:08:12 -0500

    wip

--- src/Market.java
+++ src/Market.java
@@ -26,8 +26,75 @@ public class Market
                 readHandler.getListOfPotions();
                 System.out.println("Which potion would you like to buy?");
                 int userChoice3 = scanner.nextInt();
-                itemBaseClass potion = readHandler.getListOfPotions().get(userChoice3);
+                Potion potion = readHandler.getListOfPotions().get(userChoice3);
+                if(thisHero.getGold() >= potion.cost)
+                {
+                    thisHero.getPotions().add(potion);
+                    thisHero.gold -= potion.cost;
+                    System.out.println("You bought a " + potion.name + " for " + potion.cost + " gold.");
+                }
+                else
+                {
+                    System.out.println("You don't have enough gold!");
+                }
+            }
 
+            else if(userChoice2 == 2)
+            {
+                readHandler.getListOfWeapons();
+                System.out.println("Which weapon would you like to buy?");
+                int userChoice3 = scanner.nextInt();
+                Weapon weapon = readHandler.getListOfWeapons().get(userChoice3);
+                if(thisHero.getGold() >= weapon.cost)
+                {
+                    thisHero.getWeapons().add(weapon);
+                    thisHero.gold -= weapon.cost;
+                    System.out.println("You bought a " + weapon.name + " for " + weapon.cost + " gold.");
+                }
+                else
+                {
+                    System.out.println("You don't have enough gold!");
+                }
+            }
+
+            else if(userChoice2 == 3)
+            {
+                readHandler.getListOfArmors();
+                System.out.println("Which armor would you like to buy?");
+                int userChoice3 = scanner.nextInt();
+                Armor armor = readHandler.getListOfArmors().get(userChoice3);
+                if(thisHero.getGold() >= armor.cost)
+                {
+                    thisHero.getArmors().add(armor);
+                    thisHero.gold -= armor.cost;
+                    System.out.println("You bought a " + armor.name + " for " + armor.cost + " gold.");
+                }
+                else
+                {
+                    System.out.println("You don't have enough gold!");
+                }
+            }
+
+            else if(userChoice2 == 4)
+            {
+                // check if hero is a sorcerer or paladin
+                if(thisHero.type == Characters.Hero.heroType.SORCERER || thisHero.type == Characters.Hero.heroType.PALADIN)
+                {
+                    ArrayList<Characters.Spell> lightningSpells = readHandler.getListOfLightningSpells();
+                    ArrayList<Characters.Spell> fireSpells = readHandler.getListOfFireballSpells();
+                    ArrayList<Characters.Spell> iceSpells = readHandler.getListOfIceSpells();
+                    System.out.println("Which type of spell would you like to buy?");
+                    System.out.println("1. Lightning" + " " + "2. Fire" + " " + "3. Ice");
+                    int userChoice3 = scanner.nextInt();
+                    if(userChoice3==1)
+                    {
+
+                    }
+                }
+                else
+                {
+                    System.out.println("You can't buy spells!");
+                }
             }
         }
     }
